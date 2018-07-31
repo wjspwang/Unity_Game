9a3614b740925f2728d36ddb47b43d77bf662244
Weaven <havenfalls123@gmail.com>
2018-07-31 19:08:58 +0800

    ABC
    update july 31

---
+++ New Unity Project/Assets/bg/GetAndSetText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetAndSetText : MonoBehaviour {
+
+    public InputField username;
+    public Text output;
+
+
+    public void GetAndSet()
+    {
+        output.text = "Hi " + username.text + "!";
+    }
+}
